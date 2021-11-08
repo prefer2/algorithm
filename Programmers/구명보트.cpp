1d23e7a5ac6be7ec0e4af43a24366d2b74652012
prefer2 <sunho6200@gmail.com>
2021-11-08 09:41:55 +0900

    구명 보트 (level2)

---
+++ Programmers/구명보트.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int start = 0;
+    int end = people.size()-1;
+    sort(people.begin(), people.end());
+    
+    while(start<=end){
+        if(people[start]+people[end]<=limit){
+            start++;
+            end--;
+            answer++;
+        }
+        else{
+            end--;
+            answer++;
+        }
+    }
+    return answer;
+}
